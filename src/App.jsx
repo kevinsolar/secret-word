5c9b63122b5c5e8fc8946418491decdfc2f55ad6
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-02 18:58:32 -0300

    Nessa aula, criamos os componentes que iremos utilizar nesse projeto, iniciamos os estagios que serao os gatilhos para rendereizar uma "pagina" correspondente.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+// Css
 import './App.css'
-import StartScreen from './components/StartScreen'
+
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+//Dados
+import { wordsList } from './data/words.js';
+
+//Componentes
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+   {id: 1, name: "start"},
+   {id: 2, name: "game"},
+   {id: 3, name: "end"},
+];
 
 function App() {
+   const [gameStage, setGameStage] = useState(stages[0].name);
+   const [words] = useState(wordsList);
 
    return (
       <div className='App'>
-         <StartScreen />
+         { gameStage === 'start' && <StartScreen /> }
+         { gameStage === 'game' && <Game /> }
+         { gameStage === 'end' && <GameOver /> }
       </div>
    )
 }
