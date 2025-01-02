561183cd9cb3b7baa9e2eac422a5987c1a8dfc52
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-02 00:04:13 -0300

    Criando meu projeto!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Secret Word
+
+Esse projeto está sendo feito com base no curso de React do -> Hora de Codar.
+
+## *Estou utilizando um esquema de estilização original, então nem tudo será igual ao do curso.
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+   fruta: ["Banana", "Maçã", "Uva", "Pera", "Melancia"],
+   corpo: ["Cabeça", "Braço", "Perna", "Mão", "Pé"],
+   computador: ["Teclado", "Mouse", "Monitor", "Gabinete", "CPU"],
+   programação: ["Javascript", "Python", "Java", "C#", "PHP"],
+   alimento: ["Arroz", "Feijão", "Carne", "Peixe", "Frango"],
+};
