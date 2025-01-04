5eadfad17d73161caa32bb0c69d47b5ff254c072
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-04 20:27:01 -0300

    Adiciona estilos e estrutura ao componente de jogo, incluindo exibição de pontuação, dicas e interação para adivinhar letras.

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -2,9 +2,33 @@ import './Game.css';
 
 const Game = ({ verifyLetter }) => {
    return (
-      <div>
-         <h2>Game</h2>
-         <button className="btn-default" onClick={verifyLetter}>Finalize o jogo</button>
+      <div className='game'>
+         <p className="points">
+            <span>Pontuação: 000</span>
+         </p>
+         <h1>Advinhe a palavra:</h1>
+         <h3 className='tip'>
+            Dica: <span>...</span>
+         </h3>
+         <p>Voce ainda tem XX tentativa(s)...</p>
+         <div className="wordContainer">
+            <span className='letter'>A</span>
+            <span className='blankSquare'></span>
+         </div>
+
+         <div className="letterContainer">
+            <p>Tente advinhar uma letra da palavra:</p>
+            <form>
+               <input type="text" name='letter' maxLength="1" placeholder='_' required />
+               <button className="btn-default">Tentar</button>
+            </form>
+         </div>
+
+         <div className="wrongLetter">
+            <p>Letras utilizadas:</p>
+            <span>a, </span>
+            <span>b, </span>
+         </div>
       </div>
    );
 };
