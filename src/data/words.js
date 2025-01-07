9e230ceaeaa77b74904b91867d3d4b0c5100b2ec
Kevin BTA <kevinleitesolar@hotmail.com>
2025-01-07 16:27:42 -0300

    adicionando mais palavras

--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,14 @@
 export const wordsList = {
-   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-   fruta: ["Banana", "Maçã", "Uva", "Pera", "Melancia"],
-   corpo: ["Cabeça", "Braço", "Perna", "Mão", "Pé"],
-   computador: ["Teclado", "Mouse", "Monitor", "Gabinete", "CPU"],
-   programação: ["Javascript", "Python", "Java", "C#", "PHP"],
-   alimento: ["Arroz", "Feijão", "Carne", "Peixe", "Frango"],
+   carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Retrovisor", "Farol", "Cambio", "Roda"],
+   fruta: ["Banana", "Maçã", "Uva", "Pera", "Melancia", "Abacaxi", "Morango", "Laranja", "Mamão", "Coco"],
+   corpo: ["Cabeça", "Braço", "Perna", "Mão", "Pé", "Olho", "Nariz", "Boca", "Orelha", "Cotovelo"],
+   computador: ["Teclado", "Mouse", "Monitor", "Gabinete", "CPU", "Memória", "Placa-Mãe", "Fonte", "HD", "SSD"],
+   programação: ["Javascript", "Python", "Java", "C#", "PHP", "HTML", "CSS", "Ruby", "SQL", "TypeScript"],
+   alimento: ["Arroz", "Feijão", "Carne", "Peixe", "Frango", "Macarrão", "Batata", "Cenoura", "Queijo", "Pão"],
+   esportes: ["Futebol", "Basquete", "Vôlei", "Tênis", "Natação", "Atletismo", "Surfe", "Ciclismo", "Golfe", "Boxe"],
+   música: ["Violão", "Guitarra", "Bateria", "Piano", "Flauta", "Microfone", "Orquestra", "Trompete", "Baixo", "Saxofone"],
+   animais: ["Cachorro", "Gato", "Elefante", "Leão", "Tigre", "Pássaro", "Peixe", "Cavalo", "Urso", "Coelho"],
+   cores: ["Vermelho", "Azul", "Verde", "Amarelo", "Laranja", "Roxo", "Preto", "Branco", "Cinza", "Marrom"],
+   países: ["Brasil", "Canadá", "Japão", "Itália", "Alemanha", "França", "México", "China", "Índia", "Austrália"],
+   filmes: ["Titanic", "Avatar", "Matrix", "Inception", "Frozen", "Interestelar", "Coringa", "Vingadores", "Harry Potter", "Jurassic Park"]
 };
